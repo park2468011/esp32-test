ef318a10d7673de9864b534960a34014a89a433b
SANGYUN PARK <park2468011@gmail.com>
2026-09-07 20:13:59 +0900

    주석추가

--- test1/test1.ino
+++ test1/test1.ino
@@ -1,7 +1,7 @@
 #define LED_PIN 5    // LED 연결된 GPIO 5번 사용
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);   // LED 핀을 출력으로 설정
+  pinMode(LED_PIN, OUTPUT);   // LED 핀을 출력으로 설정  //테스트용 주석
 }
 
 void loop() {
